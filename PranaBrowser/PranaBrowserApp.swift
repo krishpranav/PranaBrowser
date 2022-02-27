02fa42f203ade33ead507e039c59c709555fb785
Krisna Pranav <krisna.pranav@gmail.com>
2022-02-27 12:01:23 +0530

    pranaBRowser

---
+++ PranaBrowser/PranaBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PranaBrowserApp.swift
+//  PranaBrowser
+//
+//  Created by KrisnaPranav on 27/02/22.
+//
+
+import SwiftUI
+
+@main
+struct PranaBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
